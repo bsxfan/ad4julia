be024941316968de84e9ae4b8ce38119d6a231fb
bsxfan <bsxfan@gmail.com>
2013-05-07 14:05:51 +0200

    changed deprecated diagmm() to scale()
    scale is more general, can scale by vector or scalar

--- MatrixFunctionLib.jl
+++ MatrixFunctionLib.jl
@@ -4,22 +4,32 @@ trace(x::DualNum,ii...) = dualnum(trace(x.st,ii...),trace(x.di,ii...))
 diag{T<:FloatArray}(x::DualNum{T},k...) = dualnum(diag(x.st,k...),diag(x.di,k...))
 diagm{T<:FloatArray}(x::DualNum{T},k...) = dualnum(diagm(x.st,k...),diagm(x.di,k...))
 
-diagmm{X<:FloatMatrix,Y<:FloatVector}(x::DualNum{X},y::DualNum{Y}) = 
-    dualnum(diagmm(x.st,y.st),diagmm(x.di,y.st)+diagmm(x.st,y.di))
-diagmm{X<:FloatMatrix,Y<:FloatVector}(x::DualNum{X},y::Y) = 
-    dualnum(diagmm(x.st,y),diagmm(x.di,y))
-diagmm{X<:FloatMatrix,Y<:FloatVector}(x::X,y::DualNum{Y}) = 
-    dualnum(diagmm(x,y.st),diagmm(x,y.di))
 
-diagmm{X<:FloatVector,Y<:FloatMatrix}(x::DualNum{X},y::DualNum{Y}) = 
-    dualnum(diagmm(x.st,y.st),diagmm(x.di,y.st)+diagmm(x.st,y.di))
-diagmm{X<:FloatVector,Y<:FloatMatrix}(x::DualNum{X},y::Y) = 
-    dualnum(diagmm(x.st,y),diagmm(x.di,y))
-diagmm{X<:FloatVector,Y<:FloatMatrix}(x::X,y::DualNum{Y}) = 
-    dualnum(diagmm(x,y.st),diagmm(x,y.di))
 
+scale{X<:FloatMatrix,Y<:Union(FloatVector,FloatScalar)}(x::DualNum{X},y::DualNum{Y}) = 
+    dualnum(scale(x.st,y.st),scale(x.di,y.st)+scale(x.st,y.di))
+scale{X<:FloatMatrix,Y<:Union(Vector,Number)}(x::DualNum{X},y::Y) = 
+    dualnum(scale(x.st,y),scale(x.di,y))
+scale{X<:Array,Y<:FloatScalar}(x::X,y::DualNum{Y}) = 
+    dualnum(scale(x,y.st),scale(x,y.di))
+scale{X<:Matrix,Y<:FloatVector}(x::X,y::DualNum{Y}) = 
+    dualnum(scale(x,y.st),scale(x,y.di))
+
+scale{X<:union(FloatVector,FloatScalar),Y<:FloatMatrix}(x::DualNum{X},y::DualNum{Y}) = 
+    dualnum(scale(x.st,y.st),scale(x.di,y.st)+scale(x.st,y.di))
+scale{X<:Union(Vector,Number),Y<:FloatMatrix}(x::DualNum{X},y::Y) = 
+    dualnum(scale(x.st,y),scale(x.di,y))
+scale{X<:FloatScalar,Y<:Array}(x::DualNum{X},y::Y) = 
+    dualnum(scale(x.st,y),scale(x.di,y))
+scale{X<:FloatVector,Y<:Matrix}(x::DualNum{X},y::Y) = 
+    dualnum(scale(x.st,y),scale(x.di,y))
+
+	
 inv{T<:FloatMatrix}(x::DualNum{T}) = (y=inv(x.st);dualnum(y,-y*x.di*y))
 det{T<:FloatMatrix}(x::DualNum{T}) = (LU=lufact(x.st);y=det(LU);dualnum(y,y*dot(vec(inv(LU)),vec(x.di.'))))
+
+
+
 
 #chol
 # function logdet{T}(C::Cholesky{T})
